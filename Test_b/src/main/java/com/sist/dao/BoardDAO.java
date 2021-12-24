3a05f39f7c42d1b2729e79ac1b4a07aeabf5552c
khs17 <khs17@DESKTOP-CGMNMTI>
2021-12-24 10:39:22 +0900

    BoardDAO, GoodsDAO

---
+++ Test_b/src/main/java/com/sist/dao/BoardDAO.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+
+public class BoardDAO {
+	public int insert() {
+		System.out.println("게시물 등록 완료");
+		return 1; 
+	}
+	public int delete() {
+		System.out.println("게시물 삭제 완료");
+		return 1; 
+	}
+}
---
+++ Test_b/src/main/java/com/sist/dao/GoodsDAO.java
@@ -0,0 +1,10 @@
+package com.sist.dao;
+
+public class GoodsDAO {
+	public void insert() {
+		System.out.println("상품 등록 완료");
+	}
+	public void update() {
+		System.out.println("상품 수정 완료");
+	}
+}
